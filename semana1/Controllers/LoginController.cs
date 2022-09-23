ff62ca79c664234b2d02538a436651d22a04220a
Marvin Steven Jimenez Rodriguez <stev.199279@gmail.com>
2022-09-22 18:15:49 -0600

    Add project files.

---
+++ semana1/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace semana1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(String usuario,string password)
+        {
+            try {
+                return Content("1");
+
+            } catch (Exception ex) {
+                return Content("Ocurrio un error : "+ ex.Message);
+            }
+        }
+    }
+}
